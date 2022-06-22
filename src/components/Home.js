a5b326e51234c6ce6789f2b161c4d06e3a23d57c
kumarlokesh3132 <kumarlokesh3132@gmail.com>
2022-06-22 10:49:56 +0530

    added hoooks

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,30 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 const Home = () => {
-
-    const [blogs, setBlogs] = useState([
-        {title: "The first Blog", author: "Lokesh Kumar", body: "lorem ipsum....", id: 1 },
-        {title: "The second Blog", author: "Lokesh Kumar", body: "lorem ipsum....", id: 2 },
-        {title: "The third Blog", author: "Lokesh Kumar", body: "lorem ipsum....", id: 3 },
-        {title: "The fourth Blog", author: "Lokesh Kumar", body: "lorem ipsum....", id: 4 },
-    ]);
+    const [blogs, setBlogs] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState(null);
+    useEffect(() =>{ 
+        fetch(  'http://localhost:8000/blogs')
+        .then(res => {
+            if(!res.ok){
+                throw Error('Could not fetch data from resource');
+            }
+            return res.json();
+        })
+        .then(data => {
+            setIsLoading(false);
+            setBlogs(data);
+        })
+        .catch(err => {
+            setError(err.message);
+            setIsLoading(false);
+        });
+    },[]);    
     return ( <div className="home">
-        <BlogList blogs={blogs} title="All Blogs"/>
+        {error && <div> { error } </div>}
+         {isLoading && <div>Loading...</div>}
+         {blogs &&  <BlogList blogs={blogs} title="All Blogs"/>}
     </div> );
 }
  
