5eaaf6808b1eebf0c052093cb8e0f4b3f7c9bfdf
kumarlokesh3132 <kumarlokesh3132@gmail.com>
2022-06-24 22:06:29 +0530

    added navigation redirect

--- src/components/Create.js
+++ src/components/Create.js
@@ -1,33 +1,58 @@
 import { useState } from "react";
-
+import { useNavigate } from "react-router-dom";
 
 const Create = () => {
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
+  const [author, setAuthor] = useState('mario');
+  const [isLoading,setIsLoading] = useState(false);
+  const navigate = useNavigate();
 
-    const [title, setTitle] = useState('');
-    const [body, setBody] = useState('');
-    const [author, setAuthor] = useState('');
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        const blog = {title, body, author};
-        fetch('http://localhost:8000/blogs', {
-            method: 'POST',
-            headers: {"Content-type" : "application/json"},
-            body: JSON.stringify(blog)
-        }).then(() => {
-            console.log("blog added");
-        })
-    }
-    return ( 
-        <div className="create">
-            <h2>Creare a new Blog</h2>
-            <form onSubmit={handleSubmit}>
-            <input type="text" required placeholder="Blog Title" value={title} onChange={(e) => setTitle(e.target.value)}></input>
-            <textarea required placeholder="Body" value={body} onChange={(e) => setBody(e.target.value)}></textarea>
-            <input type="text" required placeholder="Author" value={author} onChange={(e) => setAuthor(e.target.value)}></input>
-            <button>Add Blog</button>
-            </form>
-        </div>
-     );
+  const handleSubmit = (e) => {
+    setIsLoading(true);
+    e.preventDefault();
+    const blog = { title, body, author };
+
+    fetch('http://localhost:8000/blogs/', {
+      method: 'POST',
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(blog)
+    }).then(() => {
+        setIsLoading(false);
+      console.log('new blog added');
+      navigate("/");
+    })
+  }
+
+  return (
+    <div className="create">
+      <h2>Add a New Blog</h2>
+      <form onSubmit={handleSubmit}>
+        <label>Blog title:</label>
+        <input 
+          type="text" 
+          required 
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <label>Blog body:</label>
+        <textarea
+          required
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
+        ></textarea>
+        <label>Blog author:</label>
+        <input 
+          type="text" 
+          required 
+          value={author}
+          onChange={(e) => setAuthor(e.target.value)}
+        />
+        {!isLoading && <button>Add Blog</button>}
+        {isLoading && <button>Addding...</button>}
+      </form>
+    </div>
+  );
 }
  
 export default Create;
